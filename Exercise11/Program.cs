3749c688a0ee97352930c2af87d183b44745e966
DELL <praveenuk7>
2021-07-25 13:56:57 +0530

    PLT

---
+++ Exercise11/Program.cs
@@ -0,0 +1,53 @@
+﻿//Author: PRAVEEN U K
+//Exercise: Question 11
+//Date: 25/07/2021
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit, num, rev = 0;
+
+            String[] stringarr = new String[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+            bool nonZeroFound = false;
+            int trailingZero = 0, i;
+            Console.WriteLine("Enter the number ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num > 0)
+            {
+                digit = num % 10;
+                if (nonZeroFound == false)
+                    if (digit != 0)
+                        nonZeroFound = true;
+                    else
+                        trailingZero = trailingZero + 1;
+                rev = rev * 10 + digit;
+                num = num / 10;
+            }
+
+
+            while (rev > 0)
+            {
+
+                Console.WriteLine(stringarr[(rev % 10)]);
+                rev = rev / 10;
+
+            }
+            i = 1;
+            while (i <= trailingZero)
+            {
+                Console.WriteLine("Zero");
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise17/Program.cs
@@ -0,0 +1,31 @@
+﻿//Author: PRAVEEN U K
+//Exercise: Question 17
+//Date: 25/07/2021
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 7, count = 1;
+            Console.WriteLine("The multiples of 7 with remainder 1 when divided by 2,3,4,5 and 6 are\n");
+            while (count <= 4)
+            {
+                if ((i % 2 == 1) && (i % 3 == 1) && (i % 4 == 1) && (i % 5 == 1) && (i % 6 == 1))
+                {
+                    if (count != 3)
+                        Console.WriteLine($"\nThe multiple number {count} is {i}");
+                    count = count + 1;
+                }
+                i = i + 7;
+            }
+            Console.ReadLine();
+        }
+    }
+}
